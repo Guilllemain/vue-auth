24ea8ad9508cef576a15c2520c374ab56889dfe5
Yann <devguillemain@gmail.com>
2019-02-19 17:16:36 +0100

    Configuring firestone

---
+++ src/components/dashboard/dashboard.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="dashboard">
+    <h1>That's the dashboard!</h1>
+    <p>You should only get here if you're authenticated!</p>
+    <p>Your email address : {{ email }}</p>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+
+  export default {
+    data() {
+      return {
+        email: ''
+      }
+    },
+    async created() {
+      try {
+        const response = await axios.get(`users.json`);
+        console.log(response);
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  h1, p {
+    text-align: center;
+  }
+
+  p {
+    color: red;
+  }
+</style>
